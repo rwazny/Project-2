a910a7115db8b701ada0d1908c37ffba4b06d1d5
Alex Lloyd <lloyd.da89@gmail.com>
2019-02-12 18:59:33 -0500

    user auth

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,11 @@
 var db = require("../models");
+var authController = require('../controllers/authcontroller.js');
 
-module.exports = function(app) {
+
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,9 +13,12 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -23,7 +28,8 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
+
 };
