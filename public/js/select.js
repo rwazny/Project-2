02463af887b9b9f385d810903d369642b1847cb1
marajita <rajita.maharjan@gmail.com>
2019-02-16 09:14:57 -0500

    updated select.js with clickable features and code cleaned up

--- public/js/select.js
+++ public/js/select.js
@@ -1,84 +1,110 @@
 //Modal related changes
+var characterList = [];
+var $characterContainer = $("#container-character");
 $(document).ready(function() {
-  var characterList = [];
-  var $characterContainer = $("#container-character");
   $(".submit").on("click", function(event) {
     console.log("MODAL CLICKED");
     event.preventDefault();
-    $.get("/api/characters", function(data) {
-      characterList = data;
-      console.log(characterList);
-      initializeRows();
-      $("#playerSelectModelId").modal("show");
-    });
+    // refresh character list based on latest attributes from the database table
+    refreshCharacters();
   });
-  function initializeRows() {
-    $characterContainer.empty();
-    var rowsToAdd = [];
-    for (var i = 0; i < characterList.length; i++) {
-      rowsToAdd.push(createNewRow(characterList[i]));
-    }
-    console.log(rowsToAdd);
-    $characterContainer.prepend(rowsToAdd);
-    console.log($characterContainer);
-  }
-  function createNewRow(character) {
-    var $newInputRow = $(
-      [
-        "<span class = 'player-card' ",
-        "id = ",
-        character.id,
-        ">",
-        "<div class='well well-custom'>",
-        "<figure class='text-center font-weight-bold'>",
-        character.character_name,
-        "</figure>",
-        "<div class='charAImg'></div>",
-        "<img class='resize char-img' ",
-        "id = 'img-",
-        character.id,
-        "' ",
-        "src = '/images/",
-        character.character_name,
-        ".png'",
-        ">",
-        "<figure class='text-center font-weight-bold'>",
-        "Attack :",
-        character.attack,
-        "</figure>",
-        "<figure class='text-center font-weight-bold'>",
-        "Weapon :",
-        character.Item.item_name,
-        "</figure>",
-        "</div>",
-        "</span>"
-      ].join("")
-    );
-    return $newInputRow;
-  }
 });
+
+function refreshCharacters() {
+  $.get("/api/characters", function(data) {
+    characterList = data;
+    console.log(characterList);
+    initializeRows();
+    $("#playerSelectModelId").modal("show");
+  });
+}
+
+function initializeRows() {
+  $characterContainer.empty();
+  var rowsToAdd = [];
+  for (var i = 0; i < characterList.length; i++) {
+    rowsToAdd.push(createNewCharCard(characterList[i]));
+  }
+  console.log(rowsToAdd);
+  $characterContainer.prepend(rowsToAdd);
+  console.log($characterContainer);
+}
+
+function createNewCharCard(character) {
+  //Creating individual character
+  var playerCardSpan = $("<span>");
+  playerCardSpan.attr("class", "player-card");
+  playerCardSpan.attr("id", character.id);
+
+  var divImgWell = $("<div>");
+  divImgWell.attr("class", "well well-custom");
+
+  var figName = $("<figure>");
+  figName.attr("class", "text-center font-weight-bold");
+  figName.text(character.character_name);
+
+  var divImg2 = $("<div>");
+  divImg2.attr("class", "charAImg");
+
+  //creating img tag
+  var charImg = $("<img>");
+  charImg.attr("src", ["/images/", character.character_name, ".png"].join(""));
+  charImg.attr("id", ["img-", character.id].join(""));
+  charImg.attr("class", "resize char-img");
+  charImg.attr("activeFlag", character.activeFlag);
+
+  //if currently active/selected, disable it
+  if (character.activeFlag === "Y") {
+    charImg.css("border", "16px solid lightgray");
+    charImg.css("filter", "grayscale(100%)");
+  }
+
+  var figName2 = $("<figure>");
+  figName2.attr("class", "text-center font-weight-bold");
+  figName2.text("Attack :" + character.attack);
+
+  var figName3 = $("<figure>");
+  figName3.attr("class", "text-center font-weight-bold");
+  figName3.text("Weapon :" + character.Item.item_name);
+
+  divImg2.prepend(charImg);
+  divImgWell.prepend(figName3);
+  divImgWell.prepend(figName2);
+  divImgWell.prepend(divImg2);
+  divImgWell.prepend(figName);
+  playerCardSpan.prepend(divImgWell);
+
+  return playerCardSpan;
+}
 
 $(document).on("click", ".char-img", function() {
   //alert("image clicked" + $(this).attr("id"));
+
   var idstr = $(this).attr("id");
   var idarr = idstr.split("-");
+  var imgFlag = $(this).attr("activeFlag");
+  var flagToset = "";
+
+  // setting activeFlag based on current flag
+  if (imgFlag === "Y") {
+    flagToset = "N";
+  } else {
+    flagToset = "Y";
+  }
   var character = {
-    activeFlag: "Y",
+    activeFlag: flagToset,
     id: idarr[1]
   };
-  updatePost(character);
+  //updating active flag to database, character table
+  updateActiveFlag(character);
 });
 
-function updatePost(character) {
+function updateActiveFlag(character) {
   $.ajax({
     method: "PUT",
     url: "/api/characters",
     data: character
   }).then(function() {
-    updateCharacter(character.id);
+    refreshCharacters();
   });
-}
-function updateCharacter(id) {
-  var imgId = "#img-" + id;
-  $(imgId).css("border", "16px solid lightgray");
 }
